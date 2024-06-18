753c907b7a4d5b67b00d45efbcfb54d2e497af05
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-18 17:00:37 +0530

    Routing done

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', ()=>{})
+router.get('/', (req,res)=>{
+    res.json({msg:'GET all workouts'})
+})
 
-router.get('/hello', ()=>{})
+router.get('/:id', (req,res)=>{
+    res.json({msg:'GET a single workout.'})
+})
+
+router.post('/:id', (req,res)=>{
+    res.json({msg:'POST a new workout.'})
+})
+
+router.delete('/:id', (req,res)=>{
+    res.json({msg:'DELETE a workout.'})
+})
+
+router.patch('/:id', (req,res)=>{
+    res.json({msg:'UPDATE a workout.'})
+})
 
 module.exports=router;
