086733099a327f478ab1df6d07ef5871589c3368
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-19 17:13:28 +0530

    model & schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout= require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -10,8 +11,14 @@ router.get('/:id', (req,res)=>{
     res.json({msg:'GET a single workout.'})
 })
 
-router.post('/', (req,res)=>{
-    res.json({msg:'POST a new workout.'})
+router.post('/', async (req,res)=>{
+    const {title,reps,load}= req.body
+    try {
+        const workout = await Workout.create({title,reps,load})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error:error.message})
+    }
 })
 
 router.delete('/:id', (req,res)=>{
