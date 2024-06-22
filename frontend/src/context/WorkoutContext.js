3553c9c2e3af6a0ddaca90119044c07dee54d8f7
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-22 14:37:04 +0530

    delete function added in react

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -5,13 +5,17 @@ export const WorkoutsContext = createContext()
 export const workoutsreducer= (state,action)=>{
     switch(action.type){
         case 'SET_WORKOUTS':
-            return(
-                {workouts: action.payload}
-            )
+            return{
+                workouts: action.payload
+            }
         case 'CREATE_WORKOUT':
-            return(
-                {workouts: [action.payload, ...state.workouts]}
-            )
+            return{
+                workouts: [action.payload, ...state.workouts]
+            }
+        case 'DELETE_WORKOUT':
+            return{
+                workouts: state.workouts.filter((w)=> w._id !== action.payload._id)
+            }
         default:
             return state
     }
