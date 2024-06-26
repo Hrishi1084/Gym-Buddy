7e87f9bcc6e2edada74b8b09f01eefbac6fd6fa9
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-26 14:36:12 +0530

    jwt token installed and used

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
+const createToken= (_id)=>{
+    return jwt.sign({_id},process.env.SECRET,{expiresIn: '3d'})
+}
 const loginUser = async (req, res) => {
     res.json({ mssg: 'Login' })
 }
@@ -8,7 +12,8 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({ email, user })
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
