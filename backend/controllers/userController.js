f29a4f4a8141c2e283af4b7471d65539edcd104d
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-26 17:11:44 +0530

    login added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,18 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
-const createToken= (_id)=>{
-    return jwt.sign({_id},process.env.SECRET,{expiresIn: '3d'})
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
 }
 const loginUser = async (req, res) => {
-    res.json({ mssg: 'Login' })
+    const { email, password } = req.body
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 const signupUser = async (req, res) => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,4 +36,19 @@ userSchema.statics.signup = async function (email, password) {
     return user
 }
 
+userSchema.statics.login = async function (email,password){
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Incorrect Email')
+    }
+    const match = await bcrypt.compare(password,user.password)
+    if(!match){
+        throw Error('Incorrect Password')
+    }
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
