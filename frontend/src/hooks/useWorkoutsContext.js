f196b250cd14215baa0fe9e4a0a5b58e045580e2
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-26 19:17:23 +0530

    AuthContext added

--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -1,10 +1,10 @@
 import { WorkoutsContext } from "../context/WorkoutContext"
 import { useContext } from "react"
 
-export const useWorkoutsContext= ()=>{
+export const useWorkoutsContext = () => {
     const context = useContext(WorkoutsContext)
-    if(!context){
-        throw Error('useWorkoutsContext must be inside an WorkoutsCOntextProvider')
+    if (!context) {
+        throw Error('useWorkoutsContext must be inside an WorkoutsContextProvider')
     }
     return context
 }
