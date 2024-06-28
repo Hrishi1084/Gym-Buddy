b3741d22833432b3729706f3312e95cda0e3a3f8
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-28 13:45:50 +0530

    hooks added to connect login & signup with backend

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,11 +1,13 @@
 import {useState} from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = ()=>{
     const [email,setEmail]= useState('')
     const [password,setPassword]= useState('')
+    const {signup,isLoading,error} = useSignup()
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        console.log(email,password)
+        await signup(email,password)
     }
     return(
         <form className='signup' onSubmit={handleSubmit}>
@@ -22,7 +24,8 @@ const Signup = ()=>{
                 onChange={(e)=>setPassword(e.target.value)}
                 value={password}
             />
-            <button>Signup</button>
+            <button disabled={isLoading}>Signup</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
