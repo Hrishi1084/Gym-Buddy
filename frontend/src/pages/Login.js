d70bcc17f3a8b0f32b0f561609848afb841afbbb
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-28 14:22:28 +0530

    useLogin hook added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,13 @@
 import {useState} from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = ()=>{
     const [email,setEmail]= useState('')
     const [password,setPassword]= useState('')
+    const {login,error,isLoading} = useLogin()
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        console.log(email,password)
+        await login(email,password)
     }
     return(
         <form className='login' onSubmit={handleSubmit}>
@@ -22,7 +24,8 @@ const Login = ()=>{
                 onChange={(e)=>setPassword(e.target.value)}
                 value={password}
             />
-            <button>Login</button>
+            <button disabled={isLoading}>Login</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     )
 }
