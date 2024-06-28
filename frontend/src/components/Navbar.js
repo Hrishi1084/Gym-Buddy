f1f3a056fe9dff6630cdd2aff8aae8cbc95e82c5
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-28 14:09:56 +0530

    Logout button added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom"
+import {useLogout} from '../hooks/useLogout'
 
 const Navbar = () => {
+    const {logout} = useLogout()
+    const handleClick= ()=>{
+        logout()
+    }
     return (
         <header>
             <div className="container">
@@ -8,6 +13,9 @@ const Navbar = () => {
                     <h1>Gym Buddy</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
