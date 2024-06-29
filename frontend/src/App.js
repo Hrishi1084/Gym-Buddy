a02cada36ac23860894d1618678a4a09e0a14fb2
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-06-29 17:59:51 +0530

    redirecting based on user status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+  const { user } = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
             <Route
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
